e0b5c242b28d707de4d2e19d9922b23fc2df7458
Hisham Görner <hisham30g@gmail.com>
2024-10-03 16:00:35 +0200

    wip

--- src/server.rs
+++ src/server.rs
@@ -1,8 +1,12 @@
 use std::io::Result;
 
-use axum::Router;
+use axum::{http::HeaderName, Router};
 use tokio::net::TcpListener;
-use tower_http::trace::TraceLayer;
+use tower_http::{
+    request_id::{MakeRequestUuid, PropagateRequestIdLayer, SetRequestIdLayer},
+    trace::TraceLayer,
+};
+use tracing_subscriber::fmt::layer;
 use typed_builder::TypedBuilder;
 
 use crate::{
@@ -22,11 +26,16 @@ impl Server {
     pub fn router(&self) -> Router {
         use axum::routing::get;
         use tower::ServiceBuilder;
+
+        let request_id = HeaderName::from_static("x-request-id");
+
         Router::new()
             .route("/health_check", get(health_check))
             .layer(
                 ServiceBuilder::new()
-                    .layer(TraceLayer::new_for_http().make_span_with(make_span_with)),
+                    .layer(SetRequestIdLayer::new(request_id.clone(), MakeRequestUuid))
+                    .layer(TraceLayer::new_for_http().make_span_with(make_span_with))
+                    .layer(PropagateRequestIdLayer::new(request_id)),
             )
             .with_state(self.state.clone())
     }
